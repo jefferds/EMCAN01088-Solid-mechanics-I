0b3e0a4ff3e7cfe39fe5dbcadc6b30e385bf2323
jefferds <jefferds@gmail.com>
2021-11-10 21:52:38 -0300

    Unidade 05

---
+++ Unidade 05 - Equilibrio de um corpo.jl
@@ -0,0 +1,52 @@
+### A Pluto.jl notebook ###
+# v0.16.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e9af4550-b632-11eb-1d0d-e11b45468706
+html"<button onclick=present()> Presente </button>"
+
+# ╔═╡ 4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
+md"""
+## Plano Unidade 05 - Equilíbrio de um corpo.
+
+### Tema relacionado na ementa:
+
+1. Equilíbrio de corpos rígidos.
+6. Cálculo das Reações.
+ 
+> Capítulo 4 - Equilíbrio de corpos rígidos.
+>
+> **Fonte:**  3. BEER, Ferdinand P. _**Mecânica vetorial para engenheiros : estática**_., 9ª Edição
+
+### Resultados de aprendizagem esperados
+
+✔ Analisar os esforços que atuam sobre um corpo rígido.
+
+✔ Desenvolver estratégias para solução de problemas. 
+
+✔ Estabelecer relações interpessoais e trabalhar em equipe, comunicar-se oralmente e ter responsabilidade coletiva.
+"""
+
+# ╔═╡ 64cb0563-2802-459e-8b14-2430546554ab
+md"""
+### Roteiro
+
+🕐 1. Indicação do tema da atividade a ser executada por cada grupo *(até 10/nov/2021)*,
+ 
+- _**Equilíbrio de corpos rígidos**_
+
+🕔 2. Estudar o capítuolo 4 do livro *Mecânica vetorial para engenheiros* e  apresentar dois dos subitens (4.1 à 4.9) e apresentar um exercicio de cada *(até 17/nov/2021)*;:
+
+🕙 3. Apresentações da solução dos exercícios *(até 17/nov/2021)*;
+
+🕑 4. Avaliações *(até 22/nov/2021)* e
+
+🕒 5. Feedback (sala) *(até 22/nov/2021)*.
+"""
+
+# ╔═╡ Cell order:
+# ╟─e9af4550-b632-11eb-1d0d-e11b45468706
+# ╟─4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
+# ╟─64cb0563-2802-459e-8b14-2430546554ab
